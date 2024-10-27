ffcbd8f2ba6f40a57558d43c37f61bfe20ba4ca1
Francois van Lieshout <FvLieshout@outlook.com>
2024-10-27 15:08:48 +0100

    update app.py file

--- multi-tier-vpc/app.py
+++ multi-tier-vpc/app.py
@@ -20,7 +20,7 @@ MultiTierVpcStack(app, "MultiTierVpcStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='12345678', region='us-east-1'),
+    #env=cdk.Environment(account='xxxxxxxxxxx', region='xxxxxxxxxx'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
